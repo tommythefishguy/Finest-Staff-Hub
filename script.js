0ab68d35328e115bc1fb25bb2edc88085d4b6f9c
tommythefishguy <maloneytommy83@gmail.com>
2026-04-26 12:39:00 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-# Finest Aquatics Staff Hub
-
-Upload these files to a GitHub repo, then enable GitHub Pages.
-
-Demo logins:
-- Staff PIN: 1234
-- Manager PIN: 9999
-
-This version stores data locally on the device for quick testing. For multi-device live syncing, upgrade to Firebase/Supabase cloud storage.
